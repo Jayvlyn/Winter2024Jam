b30ab822a8371eaf5785b6cb5c71459819b4b055
KFrodente <kianfrodente@gmail.com>
2024-02-23 14:14:57 -0700

    GetClosestSlashable method in NearbySlashable script

--- Assets/Kian/Scripts/NearbySlashable.cs
+++ Assets/Kian/Scripts/NearbySlashable.cs
@@ -5,15 +5,33 @@ using UnityEngine;
 
 public class NearbySlashable : Singleton<NearbySlashable>
 {
-    [SerializeField] List<GameObject> closeObjects = new List<GameObject>();
+    [SerializeField] List<Slashable> closeObjects = new List<Slashable>();
 
     public void Add(Slashable s)
     {
-
+        closeObjects.Add(s);
     }
 
     public void Remove(Slashable s)
     {
-        
+        closeObjects.Remove(s);
+    }
+
+    public Slashable GetClosestSlashable()
+    {
+        float closestDistance = 100.0f;
+        Slashable closestSlashable = null;
+
+        foreach(Slashable s in closeObjects)
+        {
+            float distance = Vector3.Distance(transform.position, s.transform.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestSlashable = s;
+            }
+        }
+
+        return closestSlashable;
     }
 }
