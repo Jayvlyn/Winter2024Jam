11d01324d10d886bf624a1a75b64ef142ea89021
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2024-02-24 00:01:40 -0700

    Sword Controller On Player Ish

--- Assets/_Project/Scripts/SwordController.cs
+++ Assets/_Project/Scripts/SwordController.cs
@@ -11,19 +11,24 @@ public class SwordController : MonoBehaviour
 
     public void Throw(Vector2 direction, Vector2 origin)
     {
+        Debug.DrawRay(origin, direction * maxThrowDistance);
         RaycastHit2D raycast = Physics2D.Raycast(origin, direction, maxThrowDistance, hitLayer);
         swordPoint.position = raycast.point;
         
         sword.ChangeState(sword.throwing);
+        realingIn = false;
     }
 
+    private bool realingIn;
     public void RealIn()
     {
+        if (realingIn) return;
         sword.ChangeState(sword.idle);
     }
 
     public float Catch(Transform playerPos)
     {
         return Vector2.Distance(playerPos.position, sword.transform.position);
+        realingIn = false;
     }
 }
