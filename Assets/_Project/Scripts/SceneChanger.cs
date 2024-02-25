2df4a56b922298bd367e29149060750303c57d72
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2024-02-25 08:38:10 -0700

    SceneGame

--- Assets/_Project/Scripts/SceneChanger.cs
+++ Assets/_Project/Scripts/SceneChanger.cs
@@ -7,8 +7,21 @@ public class SceneChanger : MonoBehaviour
 {
     [SerializeField] private string sceneName;
 
+    
     public void ChangeScene()
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void ChangeSceneSoon(float time)
+    {
+        StartCoroutine(ChangeSoon(time));
+    }
+
+    IEnumerator ChangeSoon(float time)
+    {
+        yield return new WaitForSeconds(time);
+        ChangeScene();
+
     }
 }
