adf039086529293c5784a49f512f91bb9afcb07d
KFrodente <kianfrodente@gmail.com>
2024-02-24 21:48:09 -0700

    audio manager attempt

--- Assets/Kian/Scripts/AudioManager.cs
+++ Assets/Kian/Scripts/AudioManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : Singleton<AudioManager>
 {
     private AudioSource source;
