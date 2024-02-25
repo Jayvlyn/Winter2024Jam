eef67730db390e49644ce1388bca9ed0707cb35e
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2024-02-24 21:50:35 -0700

    AudioFix

--- Assets/Kian/Scripts/AudioManager.cs
+++ Assets/Kian/Scripts/AudioManager.cs
@@ -6,24 +6,25 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class AudioManager : Singleton<AudioManager>
 {
-    private AudioSource source;
 
     [SerializeField, Range(0, 1)] private float slashDirPitchChangeAmount;
 
-    private void Start()
+    private AudioSource GetSource()
     {
-        source = GetComponent<AudioSource>();
+        return GetComponent<AudioSource>();
     }
 
 
     public void PlayOneShot(AudioClip clip)
     {
+        AudioSource source = GetSource();
         source.pitch = 1.0f;
         source.PlayOneShot(clip);
     }
 
     public void PlayOneShotAtPitch(AudioClip clip, float pitch)
     {
+        AudioSource source = GetSource();
         source.pitch = pitch;
         source.PlayOneShot(clip);
         source.pitch = 1.0f;
@@ -31,6 +32,7 @@ public class AudioManager : Singleton<AudioManager>
 
     public void PlayOneShotOnSlashDir(AudioClip clip, float yDir)
     {
+        AudioSource source = GetSource();
         source.pitch = 1.0f + (yDir * slashDirPitchChangeAmount);
         source.PlayOneShot(clip);
     }
