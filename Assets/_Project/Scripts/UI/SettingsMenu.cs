843d52232a70660ef7ff47f2295a244209fbc5a9
Jacob Leonardo <leonardojacob257@gmail.com>
2024-03-02 16:08:53 -0700

    Settings fixes

--- Assets/_Project/Scripts/UI/SettingsMenu.cs
+++ Assets/_Project/Scripts/UI/SettingsMenu.cs
@@ -14,14 +14,19 @@ public class SettingsMenu : MonoBehaviour
     public TMPro.TMP_Dropdown resolutionDropdown;
     public TMP_Text timerToggleText;
     public Toggle timerToggle;
+    public Toggle fullscreenToggle;
     private Resolution[] _resolutions;
     public GameObject speedRunTimer;
+
+    private bool settingsJustOpened = false;
 
     // Settings data
     public bool speedRunTimerOn = false;
 
     private void OnEnable()
     {
+        StartCoroutine(openTimer());
+
         SettingsData settingsData = SaveSystem.LoadSettings();
         if (settingsData != null)
         {
@@ -43,17 +48,16 @@ public class SettingsMenu : MonoBehaviour
                 timerToggleText.color = new Color(timerToggleText.color.r, timerToggleText.color.g, timerToggleText.color.b, .2f);
             }
         }
-    }
 
-    private void Start()
-    {
+        fullscreenToggle.isOn = Screen.fullScreen;
+
         int currentResIndex = 0;
         _resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
         List<string> stringRes = new List<string>();
-        for(int i = 0; i < _resolutions.Length; i++)
+        for (int i = 0; i < _resolutions.Length; i++)
         {
-            stringRes.Add($"{_resolutions[i].width} X {_resolutions[i].height}");
+            stringRes.Add($"{_resolutions[i].width} X {_resolutions[i].height} {_resolutions[i].refreshRateRatio}hz");
             if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
             {
                 currentResIndex = i;
@@ -64,10 +68,18 @@ public class SettingsMenu : MonoBehaviour
         resolutionDropdown.RefreshShownValue();
     }
 
+    private void Update()
+    {
+        Debug.Log(settingsJustOpened);
+    }
+
     public void SetRes(int index)
     {
-        Resolution resolution = _resolutions[index];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        if(!settingsJustOpened)
+        {
+            Resolution resolution = _resolutions[index];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }
     }
     public void OnBack()
     {
@@ -100,5 +112,11 @@ public class SettingsMenu : MonoBehaviour
         speedRunTimerOn = toggle;
         SaveSystem.SaveSettings(this);
     }
-    
+
+    private IEnumerator openTimer()
+    {
+        settingsJustOpened = true;
+        yield return new WaitForSecondsRealtime(0.2f);
+        settingsJustOpened = false;
+    }
 }
